5ba5c4129d0dc3926bb67d780154c04a9d18d36c
Bernard Fuentes <bernard.fuentes@weeks.com.au>
2021-03-04 20:24:37 +1030

    Random values

--- index.js
+++ index.js
@@ -15,19 +15,19 @@ function drawGrid() {
     document.getElementById('container').innerHTML = text;
 }
 
-function isValid(localBoard, coordinates, value) {
+function isValid(coordinates, value) {
     let val = value;
     if (value === undefined) {
-        val = localBoard[coordinates[0]][coordinates[1]]; 
+        val = board[coordinates[0]][coordinates[1]]; 
     }
 
     for (let i = 0; i < 9; i++) {
         // Checking row
-        if (localBoard[coordinates[0]][i] === val && coordinates[1] !== i && val !== 0) {
+        if (board[coordinates[0]][i] === val && coordinates[1] !== i && val !== 0) {
             return false
         }
         // Checking column
-        if (localBoard[i][coordinates[1]] === val && coordinates[0] !== i && val !== 0) {
+        if (board[i][coordinates[1]] === val && coordinates[0] !== i && val !== 0) {
             return false
         }
     }
@@ -37,7 +37,7 @@ function isValid(localBoard, coordinates, value) {
 
     for (let i = boxCoordinates[0]*3; i < boxCoordinates[0]*3 + 3; i++) {
         for (let j = boxCoordinates[1]*3; j < boxCoordinates[1]*3 + 3; j++) {
-            if (localBoard[i][j] === val && (coordinates[0] !== i || coordinates[1] !== j) && val !== 0) {
+            if (board[i][j] === val && (coordinates[0] !== i || coordinates[1] !== j) && val !== 0) {
                 return false
             }
         } 
@@ -51,28 +51,33 @@ function isValid(localBoard, coordinates, value) {
 function getValuesFromBoard() {
     Array.from(document.getElementsByClassName('choice')).map((cell) => {
         const coordinates = cell.name.split('-');
-        board[coordinates[0]][coordinates[1]] = cell.value === '' ? 0 : parseInt(cell.value); 
+        document.getElementById(cell.name).classList.remove('bold');
+        board[coordinates[0]][coordinates[1]] = cell.value === '' ? 0 : parseInt(cell.value);
+        if (cell.value !== '') {
+            document.getElementById(cell.name).classList.add('bold');
+        }
     });
 }
 
-function pushValuesToBoard(localBoard) {
+function pushValuesToBoard() {
     for (let i = 0; i < 9; i++) {
         for (let j = 0; j < 9; j++) {
             const id = i + '-' + j;
-            document.getElementById(id).value = localBoard[i][j]
+            document.getElementById(id).value = board[i][j]
         }
     }
 }
 
 function checkCell(i,j) {
     getValuesFromBoard();
-    if (isValid(board,[i,j])) {
+    if (isValid([i,j])) {
         Array.from(document.getElementsByClassName('choice')).map((cell) => {
             if (cell.disabled) {
                 cell.disabled = false;
             }
         });
         document.getElementById('resolve').classList.remove('opacity-50', 'cursor-not-allowed');
+        displayMessage('')
     } else {
         const id = i + '-' + j;
         Array.from(document.getElementsByClassName('choice')).map((cell) => {
@@ -81,13 +86,14 @@ function checkCell(i,j) {
             }
         });
         document.getElementById('resolve').classList.add('opacity-50', 'cursor-not-allowed');
+        displayMessage('We can\'t solve such sudoku. Please change the last entry.')
     }
 }
 
-function nextEmptyCell(localBoard) {
+function nextEmptyCell() {
     for (let i = 0; i < 9; i++) {
         for (let j = 0; j < 9; j++) {
-            if (localBoard[i][j] === 0) {
+            if (board[i][j] === 0) {
                 return [i,j]
             }
         }
@@ -95,36 +101,62 @@ function nextEmptyCell(localBoard) {
     return false
 }
 
+function displayMessage(message) {
+    document.getElementById('message').innerHTML = message;
+}
+
 function reset() {
     Array.from(document.getElementsByClassName('choice')).map((cell) => {
        cell.value = '';
        cell.disabled = false;
     });
+    displayMessage('')
+}
+
+function random() {
+    let emptyCells = [];
+    Array.from(document.getElementsByClassName('choice')).map((cell) => {
+        const coordinates = cell.name.split('-');
+        document.getElementById(cell.name).classList.remove('bold');
+        if (board[coordinates[0]][coordinates[1]] !== '') {
+            emptyCells.push(coordinates);
+        }
+    });
+    const randomCell = emptyCells[Math.floor(Math.random() * emptyCells.length)];
+    let val = 0;
+    let first = true;
+    while (first === true || !isValid(randomCell, val)) {
+        first = false;
+        val = Math.floor((Math.random() * 9) + 1);
+    }
+    const id = randomCell[0] + '-' + randomCell[1];
+    document.getElementById(id).value = val;
+    getValuesFromBoard()
+
 }
 
 function resolve() {
+    displayMessage('')
     getValuesFromBoard();
-    if (backTrakResolution(board)) {
-        pushValuesToBoard(board)
+    if (backTrakResolution()) {
+        pushValuesToBoard()
     }
 }
 
-function backTrakResolution(localBoard) {
-    let cell = nextEmptyCell(localBoard);
-    
+function backTrakResolution() {
+    let cell = nextEmptyCell();
 
     if (cell === false) {
         return true
     } 
 
     for (let i = 1; i < 10; i++) {
-        if (isValid(localBoard, cell, i)) {
-            localBoard[cell[0]][cell[1]] = i;
-            if (backTrakResolution(localBoard)) {
+        if (isValid(cell, i)) {
+            board[cell[0]][cell[1]] = i;
+            if (backTrakResolution()) {
                 return true;
             }
-            console.log(cell, i);
-            localBoard[cell[0]][cell[1]] = 0;
+            board[cell[0]][cell[1]] = 0;
         }
     }
 
@@ -134,3 +166,5 @@ function backTrakResolution(localBoard) {
 let board = Array.from(Array(9), () => new Array(9));
 
 drawGrid();
+
+displayMessage('Add manually (or randomly) some values');
